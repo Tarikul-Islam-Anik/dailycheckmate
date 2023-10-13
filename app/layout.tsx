0f331359e7e936a0c0b2ac27c7af4bbd8b54c734
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-13 12:08:55 +0600

    Initial commit.

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Daily Checkmate",
+  description: "Simple productivity app for for quick and easy daily planning",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>{children}</body>
+    </html>
+  );
+}
