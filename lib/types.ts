31dc70bd965c3ec49948726130ad3afcbbd52bd5
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-16 22:49:30 +0600

    Updated.

--- lib/types.ts
+++ lib/types.ts
@@ -9,4 +9,18 @@ export const TodoSchema = z.object({
   updatedAt: z.string(),
 });
 
+export const ReminderSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  description: z.string().optional(),
+  links: z.string().optional(),
+  deadline: z.string(),
+  reminderTime: z.string(),
+  status: z.enum(["reminder", "completed", "trash"]),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+});
+
 export type Todo = z.infer<typeof TodoSchema>;
+
+export type Reminder = z.infer<typeof ReminderSchema>;
