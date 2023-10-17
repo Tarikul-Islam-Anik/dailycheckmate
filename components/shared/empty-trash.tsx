49d2bba626448323a7ede392105256f0dc5beb5d
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-17 10:41:55 +0600

    Added alert message for double confirmation.

--- components/shared/empty-trash.tsx
+++ components/shared/empty-trash.tsx
@@ -5,6 +5,7 @@ import { TrashIcon } from "@heroicons/react/24/outline";
 import { Button } from "@/components/ui/button";
 import { EmptyTrash as ET } from "@/lib/actions";
 import Tooltip from "../shared/tooltip";
+import AlertDialog from "./alert-dialog";
 
 const EmptyTrash = ({ type }: { type: "todos" | "reminders" }) => {
   const [, setTodos] = useAtom(todoAtom);
@@ -22,16 +23,17 @@ const EmptyTrash = ({ type }: { type: "todos" | "reminders" }) => {
   }
 
   return (
-    <Tooltip content="Empty trash">
-      <Button
-        aria-label="Empty trash"
-        variant="destructive"
-        size="icon"
-        onClick={handleEmptyTrash}
-      >
-        <TrashIcon className="w-5 h-5 " />
-      </Button>
-    </Tooltip>
+    <AlertDialog
+      title="Are you sure?"
+      description="This action is irreversible. All the items in the trash will be deleted permanently."
+      action={handleEmptyTrash}
+    >
+      <Tooltip content="Empty trash">
+        <Button aria-label="Empty trash" variant="destructive" size="icon">
+          <TrashIcon className="w-5 h-5 " />
+        </Button>
+      </Tooltip>
+    </AlertDialog>
   );
 };
 
