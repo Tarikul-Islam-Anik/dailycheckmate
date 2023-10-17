785b200675effc8c1514188595b42b5c7aeaa093
Tarikul Islam Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-17 04:15:59 +0000

    Fetching data at root level.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import axios from "axios";
+import { useEffect } from "react";
+import { useAtom } from "jotai";
+import { todoAtom, reminderAtom } from "@/atom";
+import { Flex } from "@radix-ui/themes";
 import ReminderList from "@/components/reminders";
 import TodoList from "@/components/todos";
-import { Flex } from "@radix-ui/themes";
 
 export default function Home() {
+  const [, setTodos] = useAtom(todoAtom);
+  const [, setReminders] = useAtom(reminderAtom);
+  useEffect(() => {
+    axios.get("/api/todos").then((res) => {
+      setTodos(res.data);
+    });
+    axios.get("/api/reminders").then((res) => {
+      setReminders(res.data);
+    });
+  }, []);
+
   return (
     <Flex direction="column" width="100%" gap="9">
       <TodoList />
