290242c94f985ad5e9fe0af1998d44636655038c
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-18 15:36:20 +0600

    Sorting data.

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,28 @@ import { useEffect } from "react";
 import { useAtom } from "jotai";
 import { todoAtom, reminderAtom } from "@/atom";
 import { Flex } from "@radix-ui/themes";
+import { sortByNewest } from "@/lib/utils";
+import { Todo, Reminder } from "@/lib/types";
 import ReminderList from "@/components/reminders";
 import TodoList from "@/components/todos";
+import HabitsList from "@/components/habit";
 
 export default function Home() {
   const [, setTodos] = useAtom(todoAtom);
   const [, setReminders] = useAtom(reminderAtom);
+
   useEffect(() => {
     axios.get("/api/todos").then((res) => {
-      setTodos(res.data);
+      const sortedData = res.data.sort((a: Todo, b: Todo) => {
+        return sortByNewest(a.createdAt, b.createdAt);
+      });
+      setTodos(sortedData);
     });
     axios.get("/api/reminders").then((res) => {
-      setReminders(res.data);
+      const sortedData = res.data.sort((a: Reminder, b: Reminder) => {
+        return sortByNewest(a.deadline, b.deadline);
+      });
+      setReminders(sortedData);
     });
   }, []);
 
@@ -24,6 +34,7 @@ export default function Home() {
     <Flex direction="column" width="100%" gap="9">
       <TodoList />
       <ReminderList />
+      <HabitsList />
     </Flex>
   );
 }
