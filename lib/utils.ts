4a2960f2a1ef3bf2dba1cdbe64c31ad4befa9b0f
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-18 15:37:05 +0600

    Added two sorting functions.

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,3 +7,11 @@ export const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function sortByNewest(a: string, b: string) {
+  return new Date(b).getTime() - new Date(a).getTime();
+}
+
+export function sortByOldest(a: string, b: string) {
+  return new Date(a).getTime() - new Date(b).getTime();
+}
