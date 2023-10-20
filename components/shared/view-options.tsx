dcfc4de5789328d7bac6591f4a9e7e72828e1896
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-20 19:36:05 +0600

    Added a filter view option.

---
+++ components/shared/view-options.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState } from "react";
+import { MixerHorizontalIcon } from "@radix-ui/react-icons";
+import { Flex, Text } from "@radix-ui/themes";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const ViewOptions = () => {
+  const [filter, setFilter] = useState("in-progress");
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="text-violet-500">
+          <Flex align="center" gap="2">
+            <MixerHorizontalIcon />
+            <Text className="">View</Text>
+          </Flex>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Filter</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={filter} onValueChange={setFilter}>
+          <DropdownMenuRadioItem value="in-progress">
+            In Progress
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="completed">
+            Completed
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="trash">Trash</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ViewOptions;
