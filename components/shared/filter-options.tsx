c587f94d993565a2d854d3f1aa81bab7b5c6434a
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-21 22:51:42 +0600

    Added shared filter component.

--- components/shared/filter-options.tsx
+++ components/shared/filter-options.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useState } from "react";
 import { MixerHorizontalIcon } from "@radix-ui/react-icons";
 import { Flex, Text } from "@radix-ui/themes";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -14,24 +14,38 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-const ViewFilterOptions = () => {
-  const [filter, setFilter] = useState("in-progress");
+type status = "reminder" | "todo" | "completed" | "trash";
+
+const FilterOptions = ({
+  type,
+  className,
+  filter,
+  setFilter,
+}: {
+  type: "reminder" | "todo";
+  className?: string;
+  filter: status;
+  setFilter: (status: status) => void;
+}) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="text-violet-500">
+        <Button
+          variant="ghost"
+          className={cn("text-violet-500 font-bold", className)}
+        >
           <Flex align="center" gap="2">
             <MixerHorizontalIcon />
-            <Text className="">View</Text>
+            <Text>View</Text>
           </Flex>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>Filter</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuRadioGroup value={filter} onValueChange={setFilter}>
-          <DropdownMenuRadioItem value="in-progress">
-            In Progress
+        <DropdownMenuRadioGroup value={filter} onValueChange={setFilter as any}>
+          <DropdownMenuRadioItem value={type}>
+            {type === "reminder" ? "Upcoming" : "In Progress"}
           </DropdownMenuRadioItem>
           <DropdownMenuRadioItem value="completed">
             Completed
@@ -43,4 +57,4 @@ const ViewFilterOptions = () => {
   );
 };
 
-export default ViewFilterOptions;
+export default FilterOptions;
