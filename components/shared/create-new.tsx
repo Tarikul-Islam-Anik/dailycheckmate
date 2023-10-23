62caf0c3f1fd273ed745e67b6946f13a0ce64d28
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-23 19:31:36 +0600

    Resolved import issue.

--- components/shared/create-new.tsx
+++ components/shared/create-new.tsx
@@ -2,7 +2,12 @@
 import React, { useState } from "react";
 import { Text } from "@radix-ui/themes";
 import { buttonVariants } from "@/components/ui/button";
-import { PlusIcon } from "@heroicons/react/24/outline";
+import {
+  PlusIcon,
+  CalendarIcon,
+  CheckCircleIcon,
+  HeartIcon,
+} from "@heroicons/react/24/outline";
 import {
   Dialog,
   DialogContent,
@@ -11,28 +16,108 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import TooltipParent from "./tooltip-parent";
 import TodoForm from "../todos/todo-form";
 import ReminderForm from "../reminders/reminder-form";
+import { Button } from "@/components/ui/button";
+import HabitForm from "../habits/habit-form";
 
-const CreateNew = ({
-  type,
-  title,
-  description,
-}: {
-  type: "reminders" | "todos";
-  title: string;
-  description: string;
-}) => {
+const itemInfo = [
+  {
+    type: "todos",
+    title: "✏ Create a new task",
+    description: "You can edit and view your tasks later.",
+  },
+  {
+    type: "reminders",
+    title: "📅 Add a reminder",
+    description: "You can edit and view your reminders later.",
+  },
+  {
+    type: "habits",
+    title: "💖 Practice a habit",
+    description: "You can not edit it later.",
+  },
+];
+
+const CreateNew = () => {
   const [open, setOpen] = useState(false);
+  const [formType, setFormType] = useState("");
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <TooltipParent content="Create new">
-        <DialogTrigger
-          className={buttonVariants({ variant: "outline", size: "icon" })}
-        >
-          <PlusIcon className="w-5 h-5" />
-        </DialogTrigger>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon">
+            <PlusIcon className="w-6 h-6" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Add a new</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DialogTrigger asChild>
+            <DropdownMenuItem onSelect={() => setFormType("todos")}>
+              <CheckCircleIcon className="w-5 h-5 mr-2" />
+              <Text as="span">Task</Text>
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DialogTrigger asChild>
+            <DropdownMenuItem onSelect={() => setFormType("reminders")}>
+              <CalendarIcon className="w-5 h-5 mr-2" />
+              <Text as="span">Reminder</Text>
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DialogTrigger asChild>
+            <DropdownMenuItem onSelect={() => setFormType("habits")}>
+              <HeartIcon className="w-5 h-5 mr-2" />
+              <Text as="span">Habit</Text>
+            </DropdownMenuItem>
+          </DialogTrigger>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            <Text as="span">
+              {itemInfo.find((item) => item.type === formType)?.title}
+            </Text>
+          </DialogTitle>
+          <DialogDescription>
+            <Text as="span">
+              {itemInfo.find((item) => item.type === formType)?.description}
+            </Text>
+          </DialogDescription>
+        </DialogHeader>
+        {formType === "todos" ? (
+          <TodoForm setOpen={setOpen} />
+        ) : formType === "reminders" ? (
+          <ReminderForm setOpen={setOpen} />
+        ) : (
+          <HabitForm setOpen={setOpen} />
+        )}
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateNew;
+
+{
+  /* <TooltipParent content="Create new">
+      
       </TooltipParent>
       <DialogContent>
         <DialogHeader>
@@ -43,11 +128,10 @@ const CreateNew = ({
             <Text as="span">{description}</Text>
           </DialogDescription>
         </DialogHeader>
-        {type === "todos" ? <TodoForm setOpen={setOpen} /> : <ReminderForm setOpen={setOpen} />
+        {type === "todos" ? (
+          <TodoForm setOpen={setOpen} />
+        ) : (
+          <ReminderForm setOpen={setOpen} />
+        )}
+      </DialogContent> */
 }
-      </DialogContent>
-    </Dialog>
-  );
-};
-
-export default CreateNew;
