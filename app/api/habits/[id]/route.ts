c091bc2dbb5d72ea1d713976b55a0f37be534d28
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-23 19:30:10 +0600

    New api routes.

---
+++ app/api/habits/[id]/route.ts
@@ -0,0 +1,51 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../auth/[...nextauth]/route";
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user?.email) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  await prisma.habits.update({
+    where: { id: params.id, userId: session.user.id },
+    data: {
+      days: {
+        push: new Date(),
+      },
+    },
+  });
+  return NextResponse.json(
+    { message: "Habit status has been changed" },
+    { status: 200 }
+  );
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user?.email) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  await prisma.habits.delete({
+    where: {
+      userId: session.user.id,
+      id: params.id,
+    },
+  });
+
+  return NextResponse.json(
+    { message: "Deleted habit" },
+    { status: 201 }
+  );
+}
---
+++ app/api/habits/new/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../auth/[...nextauth]/route";
+
+export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user?.email) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  const { title } = await request.json();
+
+  const habit = await prisma.habits.create({
+    data: {
+      userId: session.user.id,
+      title,
+    },
+  });
+
+  return NextResponse.json(habit, { status: 200 });
+}
