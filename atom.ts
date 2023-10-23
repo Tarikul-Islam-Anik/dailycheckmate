b90d3a72e03889f6e56f66ea81060ef9b4812fb9
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-23 19:21:02 +0600

    Moved to lib folder.

--- atom.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "jotai";
-import { Todo, Reminder, Habits } from "./lib/types";
-
-const todoAtom = atom<Todo[]>([]);
-const reminderAtom = atom<Reminder[]>([]);
-const habitAtom = atom<Habits[]>([]);
-
-export { todoAtom, reminderAtom, habitAtom };
