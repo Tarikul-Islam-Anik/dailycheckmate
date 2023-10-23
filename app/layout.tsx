cc255be6563e2f9f98b1ce22131a658cb38faae2
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-23 21:06:53 +0600

    Moved providers to a separate component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
-"use client";
-
 import "./globals.css";
 import "./theme-config.css";
 import "@radix-ui/themes/styles.css";
-import { Grid, Theme } from "@radix-ui/themes";
+import { Metadata } from "next";
 import { Nunito } from "next/font/google";
-import { Provider } from "jotai";
 import { Toaster } from "sonner";
-import { ThemeProvider } from "@/components/shared/theme-provider";
-import { SessionProvider } from "next-auth/react";
+import Providers from "@/components/shared/providers";
 
 const nunito = Nunito({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-nunito",
 });
+
+export const metadata: Metadata = {
+  title: "Daily Checkmate",
+  description: "Simple productivity app for for quick and easy daily planning",
+  metadataBase: new URL("https://dailycheckmate.vercel.app/"),
+};
 
 export default function RootLayout({
   children,
@@ -31,21 +33,10 @@ export default function RootLayout({
         />
       </head>
       <body className={nunito.variable}>
-        <SessionProvider>
-          <Provider>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-              <Theme className="h-screen">
-                {children}
-                <Toaster />
-              </Theme>
-            </ThemeProvider>
-          </Provider>
-        </SessionProvider>
+        <Providers>
+          {children}
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
---
+++ components/shared/providers.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Provider } from "jotai";
+import { SessionProvider } from "next-auth/react";
+import { Theme } from "@radix-ui/themes";
+import { ThemeProvider } from "@/components/shared/theme-provider";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <SessionProvider>
+      <Provider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Theme className="h-screen">{children}</Theme>
+        </ThemeProvider>
+      </Provider>
+    </SessionProvider>
+  );
+};
+
+export default Providers;
