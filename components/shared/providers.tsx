e5b4cc2c1fd22ffe9a517b41e2ed9043d1eb829d
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-23 21:44:03 +0600

    SessionProvider is moved to layouts.

--- components/shared/providers.tsx
+++ components/shared/providers.tsx
@@ -1,24 +1,21 @@
-'use client';
+"use client";
 
 import { Provider } from "jotai";
-import { SessionProvider } from "next-auth/react";
 import { Theme } from "@radix-ui/themes";
 import { ThemeProvider } from "@/components/shared/theme-provider";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SessionProvider>
-      <Provider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Theme className="h-screen">{children}</Theme>
-        </ThemeProvider>
-      </Provider>
-    </SessionProvider>
+    <Provider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Theme className="h-screen">{children}</Theme>
+      </ThemeProvider>
+    </Provider>
   );
 };
 
