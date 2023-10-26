2b43323cb7da6ce48cb636208afd822566bfc3ba
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-26 16:48:08 +0600

    Refactor.

---
+++ app/components/home.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import axios from "axios";
+import { useEffect } from "react";
+import { useAtom } from "jotai";
+import { Grid, Box } from "@radix-ui/themes";
+import { todoAtom, reminderAtom, habitAtom } from "@/lib/atom";
+import LeftColumn from "./left-column";
+import RightColumn from "./right-column";
+import MiddleColumn from "./middle-column";
+import { sortByNewest } from "@/lib/utils";
+import { Todo, Reminder } from "@/lib/types";
+import useMediaQuery from "@/lib/hooks/use-media-query";
+import Message from "@/components/shared/message";
+
+export default function Home() {
+  const [, setTodos] = useAtom(todoAtom);
+  const [, setReminders] = useAtom(reminderAtom);
+  const [, setHabits] = useAtom(habitAtom);
+  const { isDesktop, width } = useMediaQuery();
+
+  useEffect(() => {
+    axios.get("/api/get-data").then((res) => {
+      const { todos, reminders, habits } = res.data;
+      setTodos(
+        todos.sort((a: Todo, b: Todo) => sortByNewest(a.createdAt, b.createdAt))
+      );
+      setReminders(
+        reminders.sort((a: Reminder, b: Reminder) =>
+          sortByNewest(a.schedule, b.schedule)
+        )
+      );
+      setHabits(habits);
+    });
+  }, []);
+
+  return isDesktop && width! > 1600 ? (
+    <Grid columns="4" height="100%">
+      <LeftColumn />
+      <Box className="col-span-2">
+        <MiddleColumn />
+      </Box>
+      <RightColumn />
+    </Grid>
+  ) : (
+    <Message
+      message="Hey 👋 there! DailyCheckmate is only optimized for desktop. Support for mobile is coming soon!"
+      className="h-screen"
+    />
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,7 @@
-"use client";
+import Home from "@/app/components/home";
 
-import axios from "axios";
-import { useEffect } from "react";
-import { useAtom } from "jotai";
-import { Grid, Box } from "@radix-ui/themes";
-import { todoAtom, reminderAtom, habitAtom } from "@/lib/atom";
-import LeftColumn from "./components/left-column";
-import RightColumn from "./components/right-column";
-import MiddleColumn from "./components/middle-column";
-import { sortByNewest } from "@/lib/utils";
-import { Todo, Reminder } from "@/lib/types";
-import useMediaQuery from "@/lib/hooks/use-media-query";
-import Message from "@/components/shared/message";
+const HomePage = () => {
+  return <Home />;
+};
 
-export default function HomePage() {
-  const [, setTodos] = useAtom(todoAtom);
-  const [, setReminders] = useAtom(reminderAtom);
-  const [, setHabits] = useAtom(habitAtom);
-  const { isDesktop, width } = useMediaQuery();
-
-  useEffect(() => {
-    axios.get("/api/get-data").then((res) => {
-      const { todos, reminders, habits } = res.data;
-      setTodos(
-        todos.sort((a: Todo, b: Todo) => sortByNewest(a.createdAt, b.createdAt))
-      );
-      setReminders(
-        reminders.sort((a: Reminder, b: Reminder) =>
-          sortByNewest(a.schedule, b.schedule)
-        )
-      );
-      setHabits(habits);
-    });
-  }, []);
-
-  return isDesktop && width! > 1280 ? (
-    <Grid columns="4" height="100%">
-      <LeftColumn />
-      <Box className="col-span-2">
-        <MiddleColumn />
-      </Box>
-      <RightColumn />
-    </Grid>
-  ) : (
-    <Message
-      message="Hey 👋 there! DailyCheckmate is only optimized for desktop. Support for mobile is coming soon!"
-      className="h-screen"
-    />
-  );
-}
+export default HomePage;
