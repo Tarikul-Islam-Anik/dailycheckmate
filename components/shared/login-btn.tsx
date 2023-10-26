351304c052192fe6c13cbcefe91e5def48e6d7cb
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-26 16:04:39 +0600

    Callback url.

--- components/shared/login-btn.tsx
+++ components/shared/login-btn.tsx
@@ -4,7 +4,14 @@ import { Button } from "../ui/button";
 
 const LoginBtn = () => {
   return (
-    <Button onClick={() => signIn("google")} className=" fixed top-4 right-16">
+    <Button
+      onClick={() =>
+        signIn("google", {
+          callbackUrl: "/",
+        })
+      }
+      className=" fixed top-4 right-16"
+    >
       Sign In
     </Button>
   );
