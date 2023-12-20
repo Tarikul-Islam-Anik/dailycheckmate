115b4a7307b125396b61208de2b2ebec29339313
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-20 11:45:02 +0600

    Update todoStatus default value

--- components/todos/index.tsx
+++ components/todos/index.tsx
@@ -1,27 +1,27 @@
 'use client';
 
 import { useState } from 'react';
-import { Flex } from '@radix-ui/themes';
+import { Box } from '@radix-ui/themes';
 import { Todo } from '@/lib/types';
 import GetTodos from './get-todos';
 import { ScrollArea } from '../ui/scroll-area';
 import FilterOptions from '../shared/filter-options';
 
 const TodoList = () => {
-  const [todoStatus, setTodoStatus] = useState<Todo['status']>('todo');
+  const [todoStatus, setTodoStatus] = useState<Todo['status']>('onGoing');
 
   return (
     <>
-      <Flex justify='end' align='center'>
+      <Box position='absolute' top='0' right='0'>
         <FilterOptions
           type='todo'
           className='text-slate-500'
           filter={todoStatus}
-          setFilter={setTodoStatus as any}
+          setFilter={setTodoStatus}
         />
-      </Flex>
+      </Box>
       <ScrollArea
-        className='h-[calc(100vh-14rem)] rounded-xl rounded-br-none rounded-tr-none pr-4'
+        className='h-[calc(100vh-16rem)] rounded-xl rounded-br-none rounded-tr-none pr-4'
         scrollHideDelay={0}
       >
         <GetTodos todoStatus={todoStatus} />
