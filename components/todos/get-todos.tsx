275cee815a624e76116fb8a9845a42e6fcff93a5
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-20 11:44:56 +0600

    Refactor filteredTodos variable and update Message component height

--- components/todos/get-todos.tsx
+++ components/todos/get-todos.tsx
@@ -1,26 +1,28 @@
 'use client';
 
 import { useAtom } from 'jotai';
-import { todoAtom } from '@/lib/atom';
-import { Todo } from '@/lib/types';
 import TodoItem from './todo-item';
+import { Todo } from '@/lib/types';
+import { todoAtom } from '@/lib/atom';
 import Message from '../shared/message';
 
-type TodoStatus = 'todo' | 'completed' | 'trash';
+type TodoStatus = 'onGoing' | 'completed' | 'trash';
 
 const GetTodos = ({ todoStatus }: { todoStatus: TodoStatus }) => {
   const [todos] = useAtom(todoAtom);
 
-  const filteredTodos = todos.filter(
+  const filteredTodos = todos?.filter(
     (todo: Todo) => todo.status === todoStatus
   );
 
-  return filteredTodos.length !== 0 ? (
-    filteredTodos.map((todo: Todo) => {
+  console.log(filteredTodos);
+
+  return filteredTodos?.length !== 0 ? (
+    filteredTodos?.map((todo: Todo) => {
       return <TodoItem key={todo.id} id={todo.id} />;
     })
   ) : (
-    <Message message='No todos found.' className='h-[calc(100vh-14rem)]' />
+    <Message message='No todos found.' className='h-[calc(100vh-16rem)]' />
   );
 };
 
